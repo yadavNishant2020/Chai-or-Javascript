38da0e456a758a1daf44639da5029d6883ddb424
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2023-10-05 16:55:43 +0000

    Data Conversion

---
+++ 01_Basics/02_data_conversion;.js
@@ -0,0 +1,22 @@
+//---- Conversion in number ------
+
+let score = true
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+//"33" => 33
+//"33abc" => Nan
+//true => 1 , false => 0
+
+// ------ Conversion into boolean 
+let isLoggedIn = "xyz"
+
+let booleanIsloggedIn = Boolean(isLoggedIn);
+console.log(booleanIsloggedIn);
+
+// 1 => true, 0 => false 
+// "" => false
+// "nsy" => true
