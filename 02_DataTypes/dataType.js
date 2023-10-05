a19eb17f7cb15ed947905e72b4f85155d6e14ea2
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2023-10-05 14:29:32 +0000

    Data types

---
+++ 02_DataTypes/dataType.js
@@ -0,0 +1,18 @@
+let name = "Nish"
+let age = 10
+let isLoggedIn = false
+
+// --------7 primitive data type-------------
+//number => 2 tp power 53
+// bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value 
+// undefined => value define ni h
+//symbols => used to give uniqueness
+
+//-------- user defined data type ----------------
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
