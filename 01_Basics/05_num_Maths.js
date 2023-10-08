992e6d2fc5ba4a6ffabefb2e7f7d66d114147825
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2023-10-08 15:02:19 +0000

    JS Numbers

---
+++ 01_Basics/05_num_Maths.js
@@ -0,0 +1,17 @@
+const score1 = 756;
+// console.log(score1);
+
+const score = new Number (756);
+// console.log(score);
+// console.log(score.toString());
+// console.log(score.toFixed(2));
+
+const otherNum = 24.23423;
+// console.log(otherNum.toPrecision(5));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//-------------------- Maths ------------------------
+console.log(Math);
