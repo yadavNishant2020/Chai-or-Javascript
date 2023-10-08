cde86af8c06b6e4e901d68194a2f19260b04c393
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2023-10-08 18:31:24 +0000

    Date and Time

---
+++ 01_Basics/06_date.js
@@ -0,0 +1,21 @@
+// Dates
+
+let myDate = new Date;
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+
+let myDate1 = new Date(2023, 0, 23);
+console.log(myDate1.toLocaleString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(myDate.getTime());
+
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
