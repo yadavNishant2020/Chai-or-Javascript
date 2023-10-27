dba96c9645723a7c332c5ba15a2d4a75cb1d94c1
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2023-10-27 18:39:11 +0000

    Function decalaration error

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -11,3 +11,17 @@ function one(){
 }
 
 one()
+
+
+//++++++++++++ Function Declaration +++++++++++++++
+
+console.log(addNum(5)); //no error 
+function addNum(Num){
+    return Num + 1;
+}
+
+// console.log(addNum2(4)); //Gives error bcoz function is holded in the variable and called before declaration
+const addNum2 = function(Num){
+    return Num + 1;
+}
+console.log(addNum2(8));
