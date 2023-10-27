574375947c6a015b46e3efedd23f04df593b62fe
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2023-10-27 18:24:28 +0000

    Scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,13 @@
+function one(){
+    const username = "NIsh";
+
+    function two(){
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+one()
