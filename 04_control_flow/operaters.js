f570c444d5dbc8678f6acc234d555a28815f69e6
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2023-10-30 19:55:52 +0000

    Operator Nullish & Ternary

---
+++ 04_control_flow/operaters.js
@@ -0,0 +1,16 @@
+//Nullish Coalescing Operator (??) : null undefined
+
+let val1;
+// val1 = 5 ?? 10;
+// val1 = null ?? 10;
+val1 = undefined ?? 10;
+
+// Ternary operator
+//condition ? true : false
+
+const teaPrice = 40;
+teaPrice <= 50 ? console.log("less then 50 ") : console.log("More");
+
+
+
+console.log(val1);
